68c3f3f49ee8353a5b79c314eaad3f008756723e
KhoeTrinh <kelvintrinh09@gmail.com>
2024-10-11 21:01:54 +0700

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, HeaderComponent],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,10 +17,15 @@ export class AppComponent {
   isDisable: boolean = false
   isActive: boolean = true
   fruitName: string = 'Apple'
+  username: string = 'Khoa'
   press(event : any) {
     console.log(event);
   }
-  press2() {
-    console.log('submit');
+  press2(user: HTMLInputElement) {
+    console.log(user.value);
+  }
+  updateUsername(username: HTMLInputElement) {
+    this.username = username.value;
+    console.log(this.username);
   }
 }
